8f857fb47f3620c633d72da35a043f06891547b8
Grozdanoff <Mir0-87@users.noreply.github.com>
2023-01-17 22:04:49 +0200

    added a program
     Data Types and Variables-Exercise

---
+++ Programing Fundamentals with C#/SnowBalls Exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+
+int num = int.Parse(Console.ReadLine());
+
+BigInteger highestVal = BigInteger.MinusOne;
+int highestSnow = 0;
+int highestTime = 0;
+int highestQuality = 0;
+
+for (int i = 0; i < num; i++)
+{
+    int snowballSnow = int.Parse(Console.ReadLine());
+    int snowballTime = int.Parse(Console.ReadLine());
+    int snowballQuality = int.Parse(Console.ReadLine());
+
+   BigInteger snowballValue = BigInteger.Pow((snowballSnow / snowballTime), snowballQuality);
+    if (highestVal < snowballValue)
+    {
+        highestVal = snowballValue;
+        highestSnow = snowballSnow;
+        highestTime = snowballTime;
+        highestQuality = snowballQuality;
+    }
+}
+Console.WriteLine($"{highestSnow} : {highestTime} = {highestVal} ({highestQuality})");
